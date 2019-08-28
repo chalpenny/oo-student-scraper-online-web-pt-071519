c1e9487f228acc20525847cf98fb57854a0b89fa
Colleen Halpenny <cp.halpenny@gmail.com>
2019-08-28 05:11:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,25 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
-class Scraper
+#name:
+#location:
+#profile_url:
+
+
+
+
+  class Scraper
+
+    empty_array = {}
 
   def self.scrape_index_page(index_url)
-    
+    html = open("https://learn-co-curriculum.github.io/student-scraper-test-page")
+    doc = Nokogiri::HTML(html)
   end
 
   def self.scrape_profile_page(profile_url)
-    
   end
 
+#  binding.pry
 end
-
